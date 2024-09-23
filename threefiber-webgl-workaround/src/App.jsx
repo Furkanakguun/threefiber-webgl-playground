9b1fd3a8e3392147a1be566304cc1968b3721022
Furkan Akgun <148264699+bfs-studio@users.noreply.github.com>
2024-09-23 17:48:14 +0300

    Sharing the same objects polyhedron

--- threefiber-webgl-workaround/src/App.jsx
+++ threefiber-webgl-workaround/src/App.jsx
@@ -1,10 +1,23 @@
 import { Canvas } from '@react-three/fiber'
-import { Box } from './Box'
+// import { Box } from './Box'
+import Polyhedron from './Polyhedron'
+import * as THREE from 'three'
+
 export default function App() {
+  const polyhedron = [
+    new THREE.BoxGeometry(),
+    new THREE.SphereGeometry(0.785398),
+    new THREE.DodecahedronGeometry(0.785398),
+  ]
+
   return (
-    <Canvas camera={{ position: [0, 0, 2] }}>
-      <Box positionTest={[-0.75, 0, 0]} name="A" />
-      <Box positionTest={[0.75, 0, 0]} name="A" />
+    <Canvas camera={{ position: [0, 0, 3] }}>
+      <Polyhedron position={[-0.75, -0.75, 0]} polyhedron={polyhedron} />
+      <Polyhedron position={[0.75, -0.75, 0]} polyhedron={polyhedron} />
+      <Polyhedron position={[-0.75, 0.75, 0]} polyhedron={polyhedron} />
+      <Polyhedron position={[0.75, 0.75, 0]} polyhedron={polyhedron} />
+      {/* <Box positionTest={[-0.75, 0, 0]} name="A" />
+      <Box positionTest={[0.75, 0, 0]} name="A" /> */}
     </Canvas>
   )
 }
